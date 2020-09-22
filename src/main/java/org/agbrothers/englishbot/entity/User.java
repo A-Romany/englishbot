626ec252be66a78c21ef13e08f1907b514bdea5f
A-Romany <65301975+A-Romany@users.noreply.github.com>
2020-09-22 22:08:14 +0300

    Feature/en 22 create user entity (#8)
    EN-22: Provide User entity

---
+++ src/main/java/org/agbrothers/englishbot/entity/User.java
@@ -0,0 +1,35 @@
+package org.agbrothers.englishbot.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+    @Id @GeneratedValue
+    @Column
+    private Long id;
+    @Column (unique = true, nullable = false)
+    private String chatId;
+    @Column
+    private String stateId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!id.equals(user.id)) return false;
+        if (!chatId.equals(user.chatId)) return false;
+        return stateId.equals(user.stateId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + chatId.hashCode();
+        result = 31 * result + stateId.hashCode();
+        return result;
+    }
+}
