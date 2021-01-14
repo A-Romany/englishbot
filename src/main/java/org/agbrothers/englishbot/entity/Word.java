584546bb5d36ede456f6a7a64f4ce0731fa05778
A-Romany <65301975+A-Romany@users.noreply.github.com>
2021-01-14 23:15:26 +0200

    EN-25: Create ukrainian-english lesson (#11)

--- src/main/java/org/agbrothers/englishbot/entity/Word.java
+++ src/main/java/org/agbrothers/englishbot/entity/Word.java
@@ -7,42 +7,18 @@ import javax.persistence.*;
 public class Word {
     @Id @GeneratedValue
     @Column
-    private Long Id;
+    private Long id;
     @Column (nullable = false)
     private String englishValue;
     @Column (nullable = false)
     private String ukrainianValue;
 
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
     public String getEnglishValue() {
         return englishValue;
     }
 
-    public void setEnglishValue(String englishValue) {
-        this.englishValue = englishValue;
-    }
-
-    public String getWordInUkrainian() {
+    public String getUkrainianValue() {
         return ukrainianValue;
-    }
-
-    public void setWordInUkrainian(String wordInUkrainian) {
-        this.ukrainianValue = wordInUkrainian;
-    }
-
-    public Word() {}
-
-    public Word(Long id, String englishValue, String ukrainianValue) {
-        Id = id;
-        this.englishValue = englishValue;
-        this.ukrainianValue = ukrainianValue;
     }
 
     @Override
@@ -52,14 +28,14 @@ public class Word {
 
         Word word = (Word) o;
 
-        if (!Id.equals(word.Id)) return false;
+        if (!id.equals(word.id)) return false;
         if (!englishValue.equals(word.englishValue)) return false;
         return ukrainianValue.equals(word.ukrainianValue);
     }
 
     @Override
     public int hashCode() {
-        int result = Id.hashCode();
+        int result = id.hashCode();
         result = 31 * result + englishValue.hashCode();
         result = 31 * result + ukrainianValue.hashCode();
         return result;
