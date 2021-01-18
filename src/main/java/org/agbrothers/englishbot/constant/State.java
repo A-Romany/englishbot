042e09b3411b6b9ae36a76e9ed3f44127271b2c1
Ahiienko Roman <roman.ahiienko@gmail.com>
2021-01-18 16:10:08 +0200

    EN-29: Implement functionality to delete a word from users's dictionary(After 2 review)

--- src/main/java/org/agbrothers/englishbot/constant/State.java
+++ src/main/java/org/agbrothers/englishbot/constant/State.java
@@ -7,6 +7,6 @@ public class State {
     public static final String UKRAINIAN_LESSON = "Ukrainian_lesson";
     public static final String LESSONS = "Lesson";
     public static final String DICTIONARY = "Dictionary";
-    public static final String ADD_WORD_TO_DICTIONARY = "Додати нове слово";
-    public static final String DELETING_WORD = "Видалити слово";
+    public static final String ADD_WORD_TO_DICTIONARY = "Adding a new word";
+    public static final String DELETING_WORD = "Deleting a word";
 }
