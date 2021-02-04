320712f16208fe125d4d9a23ac988bf134009c2f
A-Romany <65301975+A-Romany@users.noreply.github.com>
2021-02-04 13:55:09 +0200

    EN-38 fix buttons order (#20)
    EN-38 fix buttons order

--- src/main/java/org/agbrothers/englishbot/buttonsbuilder/MainMenuButtonsBuilder.java
+++ src/main/java/org/agbrothers/englishbot/buttonsbuilder/MainMenuButtonsBuilder.java
@@ -2,10 +2,11 @@ package org.agbrothers.englishbot.buttonsbuilder;
 
 import org.agbrothers.englishbot.constant.ButtonLabel;
 import org.springframework.stereotype.Component;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
 import static org.agbrothers.englishbot.constant.ButtonLabel.*;
-import static org.agbrothers.englishbot.constant.LinkLabel.*;
+import static org.agbrothers.englishbot.constant.LinkLabel.MAIN_MENU;
 
 @Component
 public class MainMenuButtonsBuilder extends ButtonsBuilder {
@@ -13,22 +14,19 @@ public class MainMenuButtonsBuilder extends ButtonsBuilder {
     @Override
     public Map<String, String> getKeyboardButtons(String messageText) {
 
-        Map<String, String> keyboardMap;
+        Map<String, String> keyboardMap = new LinkedHashMap<>();
         switch (messageText) {
             case LESSONS:
-                keyboardMap = new HashMap<>();
                 keyboardMap.put(ButtonLabel.ENGLISH, "From English to Ukrainian");
                 keyboardMap.put(ButtonLabel.UKRAINIAN, "From Ukrainian to English");
                 return keyboardMap;
             case DICTIONARY:
-                keyboardMap = new HashMap<>();
                 keyboardMap.put(PRINT_ALL_WORD, PRINT_ALL_WORD);
                 keyboardMap.put(ADD_WORD, ADD_WORD);
                 keyboardMap.put(EDIT_DICTIONARY, EDIT_DICTIONARY);
                 keyboardMap.put(REMOVE_WORD, REMOVE_WORD);
                 return keyboardMap;
             case MAIN_MENU:
-                keyboardMap = new HashMap<>();
                 keyboardMap.put(DICTIONARY, DICTIONARY);
                 keyboardMap.put(LESSONS, LESSONS);
                 return keyboardMap;
