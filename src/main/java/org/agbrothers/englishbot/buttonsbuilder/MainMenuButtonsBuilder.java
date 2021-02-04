339e39c621c838e31d2fde7d28ca600b74e82117
Ahiienko Roman <roman.ahiienko@gmail.com>
2021-02-04 13:13:46 +0200

    EN-38_Fix_buttons_order (after review)

--- src/main/java/org/agbrothers/englishbot/buttonsbuilder/MainMenuButtonsBuilder.java
+++ src/main/java/org/agbrothers/englishbot/buttonsbuilder/MainMenuButtonsBuilder.java
@@ -14,22 +14,19 @@ public class MainMenuButtonsBuilder extends ButtonsBuilder {
     @Override
     public Map<String, String> getKeyboardButtons(String messageText) {
 
-        Map<String, String> keyboardMap;
+        Map<String, String> keyboardMap = new LinkedHashMap<>();
         switch (messageText) {
             case LESSONS:
-                keyboardMap = new LinkedHashMap<>();
                 keyboardMap.put(ButtonLabel.ENGLISH, "From English to Ukrainian");
                 keyboardMap.put(ButtonLabel.UKRAINIAN, "From Ukrainian to English");
                 return keyboardMap;
             case DICTIONARY:
-                keyboardMap = new LinkedHashMap<>();
                 keyboardMap.put(PRINT_ALL_WORD, PRINT_ALL_WORD);
                 keyboardMap.put(ADD_WORD, ADD_WORD);
                 keyboardMap.put(EDIT_DICTIONARY, EDIT_DICTIONARY);
                 keyboardMap.put(REMOVE_WORD, REMOVE_WORD);
                 return keyboardMap;
             case MAIN_MENU:
-                keyboardMap = new LinkedHashMap<>();
                 keyboardMap.put(DICTIONARY, DICTIONARY);
                 keyboardMap.put(LESSONS, LESSONS);
                 return keyboardMap;
