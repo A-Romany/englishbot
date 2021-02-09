6e9a0bb91580cf1c15c8c627a59c8c6b24d81f8e
A-Romany <65301975+A-Romany@users.noreply.github.com>
2021-02-09 14:40:09 +0200

    EN-36 Refactor lesson logic (#22)
    EN-36 Refactor lesson logic

--- src/main/java/org/agbrothers/englishbot/service/LessonService.java
+++ src/main/java/org/agbrothers/englishbot/service/LessonService.java
@@ -4,10 +4,8 @@ import org.agbrothers.englishbot.entity.Lesson;
 import org.agbrothers.englishbot.entity.Word;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.security.SecureRandom;
+import java.util.*;
 
 @Service
 public class LessonService {
@@ -20,22 +18,51 @@ public class LessonService {
         this.lessonRegistry = new HashMap<>();
     }
 
-    public Lesson getLesson (String chatId){
-        if((!lessonRegistry.containsKey(chatId))||
-                (lessonRegistry.get(chatId).getCurrentWord()==null)){
+    public Lesson getLesson(String chatId) {
+        if ((!lessonRegistry.containsKey(chatId)) ||
+                (lessonRegistry.get(chatId).getCurrentWord() == null)) {
             Lesson lesson = createLesson();
-            lessonRegistry.put(chatId,lesson);
+            lessonRegistry.put(chatId, lesson);
         }
         return lessonRegistry.get(chatId);
     }
 
-    public Lesson createLesson(){
+    public Lesson createLesson() {
         List<Word> wordPool = wordPoolService.getRandomWordPool();
         List<Word> answersPool = new ArrayList<>(wordPool);
         return new Lesson(wordPool, answersPool);
     }
 
-    public void removeLesson(String chatId){
+    public void removeLesson(String chatId) {
         lessonRegistry.remove(chatId);
+    }
+
+    public Word getNextWord(Lesson lesson) {
+        Iterator<Word> iterator = lesson.getWordPool().iterator();
+        if (iterator.hasNext()) {
+            lesson.setCurrentWord(iterator.next());
+            iterator.remove();
+        } else {
+            lesson.setCurrentWord(null);
+        }
+        return lesson.getCurrentWord();
+    }
+
+    public List<Word> getAnswers(Lesson lesson) {
+        if (lesson.getCurrentWord() == null) {
+            return Collections.emptyList();
+        }
+        List<Word> answers = new ArrayList<>();
+        answers.add(lesson.getCurrentWord());
+
+        List<Word> answersPool = lesson.getAnswersPool();
+        Collections.shuffle(answersPool, new SecureRandom());
+        for (int i = 0; answers.size() < 5; i++) {
+            if (!answersPool.get(i).equals(lesson.getCurrentWord())) {
+                answers.add(answersPool.get(i));
+            }
+        }
+        Collections.shuffle(answers, new SecureRandom());
+        return answers;
     }
 }
