c482d9126ecabb2716663139a095c11e368b981b
Ahiienko Roman <roman.ahiienko@gmail.com>
2021-02-12 22:01:38 +0200

    EN-44 Implement User entity (after review2)

--- src/main/java/org/agbrothers/englishbot/repository/UserRepository.java
+++ src/main/java/org/agbrothers/englishbot/repository/UserRepository.java
@@ -2,20 +2,10 @@ package org.agbrothers.englishbot.repository;
 
 import org.agbrothers.englishbot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    @Query("SELECT chatId from User")
-    List<String> getChatIdsUser();
-
-    @Query("SELECT user FROM User user WHERE user.chatId = :chatId")
-    User getUserByChatId(@Param("chatId") String chatId);
 
     User findUserByChatId(String chatId);
 }
