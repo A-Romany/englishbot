7a768b68fad77f6d29367f439139d996f12dd1d3
Sergio <agienko.sergiy@gmail.com>
2021-03-27 18:35:34 +0200

    EN-12: add shutdown of ngrok process

--- src/main/java/org/agbrothers/englishbot/adapter/telegram/config/SetupTelegramWebHookEvent.java
+++ src/main/java/org/agbrothers/englishbot/adapter/telegram/config/SetupTelegramWebHookEvent.java
@@ -1,22 +1,19 @@
 package org.agbrothers.englishbot.adapter.telegram.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.URI;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Profile("development")
 @Component
@@ -26,19 +23,16 @@ public class SetupTelegramWebHookEvent implements ApplicationListener<ContextRef
 
     private static final String TELEGRAM_URL_TEMPLATE = "https://api.telegram.org/bot{bot_token}/setWebHook?url={bot_server_url}";
 
-    private static final String NGROK_LOCAL_URL = "http://localhost:4040/api/tunnels";
-
-    private final RestTemplate restTemplate = new RestTemplate();
-
     @Value("${telegram.token}")
     private String botToken;
 
-    @Value("${server.port}")
-    private String port;
+    private RestTemplate restTemplate;
+
+    private NgrokTunnelStarter ngrokTunnelStarter;
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        String serverUrl = getNgrokBotServerUrl();
+    public void onApplicationEvent(ContextRefreshedEvent contextStartedEvent) {
+        String serverUrl = ngrokTunnelStarter.getNgrokBotServerUrl();
 
         String url = TELEGRAM_URL_TEMPLATE.replace("{bot_token}", botToken).replace("{bot_server_url}", serverUrl);
 
@@ -49,40 +43,16 @@ public class SetupTelegramWebHookEvent implements ApplicationListener<ContextRef
             throw new RuntimeException("Setting web hook failed. Check configuration.");
         }
 
-        LOGGER.info("Web hook was set");
+        LOGGER.info("Web hook was set to " + serverUrl);
     }
 
-    private String getNgrokBotServerUrl() {
+    @Autowired
+    public void setNgrokTunnelStarter(NgrokTunnelStarter ngrokTunnelStarter) {
+        this.ngrokTunnelStarter = ngrokTunnelStarter;
+    }
 
-        String osName = System.getProperty("os.name");
-        String command = osName.toLowerCase().startsWith("windows")
-                ? "cmd /c ngrok.exe http " + port
-                : "sh -c ngrok http " + port;
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.submit(() -> {
-            try {
-                LOGGER.info("Starting ngrok tunneling");
-                Runtime.getRuntime()
-                        .exec(command, null, new File("./ngrok"));
-            } catch (IOException e) {
-                LOGGER.warn("Starting ngrok tunneling failed");
-                e.printStackTrace();
-            }
-        });
-
-        ResponseEntity<String> responseEntity = restTemplate.exchange(NGROK_LOCAL_URL, HttpMethod.GET, null, String.class);
-
-        if (responseEntity.getStatusCodeValue() != 200) {
-            throw new RuntimeException(String.format("Failed to obtain ngrok server url. Reason: %s", responseEntity.getBody()));
-        }
-
-        String ngrokUrl;
-        try {
-            ngrokUrl = new ObjectMapper().readTree(responseEntity.getBody()).get("tunnels").get(0).get("public_url").asText();
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to obtain ngrok server url.", e);
-        }
-        return ngrokUrl;
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 }
