1129b35342d35ec9f8ae18e6aaf1008159dde94f
Sergio <agienko.sergiy@gmail.com>
2021-03-27 20:28:39 +0200

    EN-52: test ci failure

--- src/test/java/org/agbrothers/englishbot/service/LessonServiceTest.java
+++ src/test/java/org/agbrothers/englishbot/service/LessonServiceTest.java
@@ -26,7 +26,7 @@ class LessonServiceTest {
         Lesson result = lessonService.getLesson(chatId);
 
         assertEquals(lesson, result);
-        verify(wordPoolService, never()).getRandomWordPool();
+        verify(wordPoolService, times(1)).getRandomWordPool();
     }
 
     @Test
