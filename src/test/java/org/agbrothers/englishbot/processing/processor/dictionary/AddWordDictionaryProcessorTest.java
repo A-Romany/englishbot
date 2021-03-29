63da069cc39984a763e9498bb13bcf8ed4a58932
A-Romany <65301975+A-Romany@users.noreply.github.com>
2021-03-29 21:18:34 +0300

    Feature/en50 add unit tests for add word dictionary processor (#27)
    EN-50: Add unit tests for AddWordDictionaryProcessor

---
+++ src/test/java/org/agbrothers/englishbot/processing/processor/dictionary/AddWordDictionaryProcessorTest.java
@@ -0,0 +1,100 @@
+package org.agbrothers.englishbot.processing.processor.dictionary;
+
+import org.agbrothers.englishbot.entity.User;
+import org.agbrothers.englishbot.entity.Word;
+import org.agbrothers.englishbot.processing.ProcessingExchange;
+import org.agbrothers.englishbot.service.DictionaryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.agbrothers.englishbot.constant.ButtonLabel.ADD_WORD;
+import static org.agbrothers.englishbot.constant.CommonPhrase.TYPED_WORD_ERROR;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class AddWordDictionaryProcessorTest {
+
+    private final DictionaryService dictionaryService = mock(DictionaryService.class);
+    private final AddWordDictionaryProcessor addWordDictionaryProcessor = new AddWordDictionaryProcessor();
+
+    private User user;
+
+    @BeforeEach
+    void init() {
+        addWordDictionaryProcessor.setDictionaryService(dictionaryService);
+        user = new User();
+    }
+
+    @Test
+    void process_messageTextEqualsAddWord_EnterWord() {
+        ProcessingExchange exchange = new ProcessingExchange(user, ADD_WORD);
+
+        addWordDictionaryProcessor.process(exchange);
+
+        String result = exchange.getResponseMessageText();
+        assertEquals("Введіть слово англійською та його переклад через пробіл", result);
+        verify(dictionaryService, never()).addWord(any(Word.class));
+    }
+
+    @Test
+    void process_receiveWordAndTranslation_AddingWord() {
+        ProcessingExchange exchange1 = new ProcessingExchange(user, "sea море");
+        ProcessingExchange exchange2 = new ProcessingExchange(user, "  fly   муха ");
+
+        when(dictionaryService.getWordByEnglishValue("sea")).thenReturn(null);
+
+        addWordDictionaryProcessor.process(exchange1);
+
+        String result1 = exchange1.getResponseMessageText();
+        assertEquals("Слово sea - море було додано до словника.", result1);
+
+        ArgumentCaptor<Word> captor = ArgumentCaptor.forClass(Word.class);
+        verify(dictionaryService).addWord(captor.capture());
+        assertEquals("sea", captor.getValue().getEnglishValue());
+
+        addWordDictionaryProcessor.process(exchange2);
+        String result2 = exchange2.getResponseMessageText();
+        assertEquals("Слово fly - муха було додано до словника.", result2);
+
+        verify(dictionaryService, times(2)).addWord(captor.capture());
+        assertEquals("fly", captor.getValue().getEnglishValue());
+
+        verify(dictionaryService, times(2)).addWord(any(Word.class));
+    }
+
+    @Test
+    void process_messageTextValidatedNewWord_NotAddingWord() {
+        ProcessingExchange exchange = new ProcessingExchange(user, "sea море");
+
+        when(dictionaryService.getWordByEnglishValue("sea")).thenReturn(new Word("sea", "море"));
+
+        addWordDictionaryProcessor.process(exchange);
+
+        String result = exchange.getResponseMessageText();
+        assertEquals("Слово sea - море вже існує у Вашому словнику.", result);
+        verify(dictionaryService, never()).addWord(any(Word.class));
+    }
+
+    @Test
+    void process_messageTextValidatedNewWord_NotValidatedWord() {
+        ProcessingExchange exchange1 = new ProcessingExchange(user, "se4a море");
+        ProcessingExchange exchange2 = new ProcessingExchange(user, "море sea");
+        ProcessingExchange exchange3 = new ProcessingExchange(user, "sea мор7е");
+
+        addWordDictionaryProcessor.process(exchange1);
+        addWordDictionaryProcessor.process(exchange2);
+        addWordDictionaryProcessor.process(exchange3);
+
+        String result1 = exchange1.getResponseMessageText();
+        assertEquals(TYPED_WORD_ERROR, result1);
+
+        String result2 = exchange2.getResponseMessageText();
+        assertEquals(TYPED_WORD_ERROR, result2);
+
+        String result3 = exchange3.getResponseMessageText();
+        assertEquals(TYPED_WORD_ERROR, result3);
+
+        verify(dictionaryService, never()).addWord(any(Word.class));
+    }
+}
